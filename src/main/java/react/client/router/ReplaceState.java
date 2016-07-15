f16d9c28b6109456c51c763131fae7d9fcab1d08
Clay Molocznik <clay@workreef.com>
2016-07-14 21:20:10 -0700

    revert gwt to previous snapshot

--- src/main/java/react/client/router/ReplaceState.java
+++ src/main/java/react/client/router/ReplaceState.java
@@ -1,11 +1,14 @@
 package react.client.router;
 
+import jsinterop.annotations.JsPackage;
+import jsinterop.annotations.JsProperty;
 import jsinterop.annotations.JsType;
 
 /**
  *
  */
-@JsType
+@JsType(isNative = true, namespace = JsPackage.GLOBAL, name = "Object")
 public class ReplaceState {
+    @JsProperty
     public String nextPathname;
 }
