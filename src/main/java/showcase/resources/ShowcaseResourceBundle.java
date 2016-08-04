35b1e7941da8e16f94c2ac1d59467e8bfaa82f53
Chris Jones <chris@axismedtech.com>
2016-08-03 20:20:23 -0700

    working

--- src/main/java/showcase/resources/ShowcaseResourceBundle.java
+++ src/main/java/showcase/resources/ShowcaseResourceBundle.java
@@ -7,6 +7,6 @@ import com.google.gwt.resources.client.TextResource;
 public interface ShowcaseResourceBundle extends ClientBundle {
     ShowcaseResourceBundle INSTANCE = GWT.create(ShowcaseResourceBundle.class);
 
-    @Source("webpack-output-prod.js")
+    @Source("webpack-output.js")
     TextResource webpackOutputJs();
 }
