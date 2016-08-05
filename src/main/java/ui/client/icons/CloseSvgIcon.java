ea224635abfd85b70a5d1d4fe4c0e72c8ea01138
Chris Jones <chris@axismedtech.com>
2016-08-04 19:16:13 -0700

    add close svg icon

---
+++ src/main/java/ui/client/icons/CloseSvgIcon.java
@@ -0,0 +1,18 @@
+package ui.client.icons;
+
+import react.client.ReactClass;
+
+import javax.inject.Inject;
+
+
+public class CloseSvgIcon extends AbstractMaterialIcon {
+
+    @Inject
+    public CloseSvgIcon() {
+    }
+
+    @Override
+    protected native ReactClass getReactClass() /*-{
+        return $wnd.MaterialUi.Icons.Close;
+    }-*/;
+}
