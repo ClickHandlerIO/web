12d316de3d4f430204102b801dd1b13a99fc85eb
Chris Jones <chris@axismedtech.com>
2017-02-17 16:34:19 -0700

    cleanup. inserting wildcard route not working

--- src/main/java/react/client/router/RootModule.java
+++ src/main/java/react/client/router/RootModule.java
@@ -42,20 +42,11 @@ public abstract class RootModule extends ModuleLoader {
         }
 
         return appRoute = new Route()
-            .path("/")
-            .component(root)
-            .getChildRoutes(
-                (partialNextState, callback) -> {
-                    RoutesCallback internal = (error, routes) -> {
-//                        Route wildcard = new Route().path("*").component(root);
-                        callback.run(error, routes);
-                    };
-                    handle(partialNextState.location.getPathname(), partialNextState.location, internal);
-                })
-            .onEnter(
-                (nextState, replaceState) -> routeGatekeeper.get().onEnter(rootRoute, nextState, replaceState))
-            .onLeave(
-                () -> routeGatekeeper.get().onLeave(rootRoute));
+                .path("/")
+                .component(root)
+                .getChildRoutes((partialNextState, callback) -> handle(partialNextState.location.getPathname(), partialNextState.location, callback))
+                .onEnter((nextState, replaceState) -> routeGatekeeper.get().onEnter(rootRoute, nextState, replaceState))
+                .onLeave(() -> routeGatekeeper.get().onLeave(rootRoute));
     }
 
     public void start(String elementId) {
